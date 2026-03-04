36f9cb4a2cc8939250ab7f67933ae66022bf2eb0
이경미 <kmee72@naver.com>
2026-03-05 02:27:26 +0900

    update ex13

--- springboot/Ex13VMDBB/src/main/resources/db.sql
+++ springboot/Ex13VMDBB/src/main/resources/db.sql
@@ -1,9 +1,31 @@
-CREATE DATABASE IF NOT EXISTS vm2;
+-- 파일이름 : db.sql
+
+-- db 생성
+DROP DATABASE IF EXISTS vm2;
+CREATE DATABASE vm2;
+
+-- db 사용 설정
 USE vm2;
 
-CREATE TABLE product (
-                         product_no INT AUTO_INCREMENT PRIMARY KEY,
-                         product_name VARCHAR(255) NOT NULL,
-                         product_price INT NOT NULL,
-                         product_limit_date DATE DEFAULT (CURRENT_DATE)
+-- 회원정보 테이블
+DROP TABLE if EXISTS vm2.product;
+CREATE TABLE vm2.product(
+                            product_no INT AUTO_INCREMENT NOT NULL PRIMARY KEY, -- 고유키
+                            product_name VARCHAR(255) NOT NULL, -- 상품이름
+                            product_price INT NOT NULL, -- 상품가격
+                            product_limit_date DATE DEFAULT (CURRENT_DATE) -- 유통가능일자
 );
+
+-- 테이블 구조 확인
+DESCRIBE vm2.product;
+-- 데이터 추가
+insert INTO vm2.product values (0, '불고기버거', 1000, '2023-12-25');
+
+-- 데이터 조회
+SELECT * FROM vm2.product;
+
+-- 데이터 수정
+UPDATE vm2.product SET product_price = 2000 WHERE product_no = 1;
+
+-- 데이터 조회
+SELECT * FROM vm2.product;
